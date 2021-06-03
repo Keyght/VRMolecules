3cbaefd48fd4d6a324316a568fc18c8c890e352e
Marina <mbakanova2016@yandex.ru>
2021-06-03 21:32:54 +0300

    ready project

---
+++ Assets/Scripts/OpeninDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeninDoor : MonoBehaviour {
+
+	public GameObject screenToClose;
+	public GameObject door1;
+	public GameObject door2;
+	public GameObject door3;
+	public GameObject door4;
+
+	public void Open()
+	{
+		screenToClose.SetActive(false);
+		door1.SetActive(true);
+		door2.SetActive(true);
+		door3.SetActive(true);
+		door4.SetActive(true);
+	}
+}
